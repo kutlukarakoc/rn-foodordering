7d219761bb52787385b8a3fd05120efab2b4fe99
kutlukarakoc <karakockutlu@gmail.com>
2024-03-27 01:14:44 +0300

    refactor: updated the way of import products

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,6 +1,6 @@
 import { View } from 'react-native';
 import { ProductListItem } from '@/components/ProductListItem';
-import products from '../../../assets/data/products';
+import products from 'assets/data/products';
 
 export default function MenuScreen() {
   return (

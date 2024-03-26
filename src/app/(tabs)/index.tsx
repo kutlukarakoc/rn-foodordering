197150d362c415ba545973b0eb0f5a103cb6846f
kutlukarakoc <karakockutlu@gmail.com>
2024-03-27 01:23:25 +0300

    style: added styles for ProductListItem containers

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,12 +1,15 @@
-import { View } from 'react-native';
+import { FlatList } from 'react-native';
 import { ProductListItem } from '@/components/ProductListItem';
 import products from 'assets/data/products';
 
 export default function MenuScreen() {
   return (
-    <View>
-      <ProductListItem product={products[0]} />
-      <ProductListItem product={products[1]} />
-    </View>
+    <FlatList
+      data={products}
+      renderItem={({ item }) => <ProductListItem product={item} />}
+			numColumns={2}
+			contentContainerStyle={{ gap: 10, padding: 10 }}
+			columnWrapperStyle={{ gap: 10 }}
+    />
   );
 }
--- src/components/ProductListItem.tsx
+++ src/components/ProductListItem.tsx
@@ -27,6 +27,7 @@ const styles = StyleSheet.create({
     backgroundColor: 'white',
     padding: 10,
     borderRadius: 20,
+		flex: 1,
   },
   image: {
     width: '100%',

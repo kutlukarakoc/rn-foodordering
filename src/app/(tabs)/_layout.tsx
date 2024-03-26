b80f0101850ff3fd9aaa2f6ea3bb87ae99ad4645
kutlukarakoc <karakockutlu@gmail.com>
2024-03-27 01:51:27 +0300

    feat: updated tabs, icons

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -7,12 +7,17 @@ import Colors from '@/constants/Colors';
 import { useColorScheme } from '@/components/useColorScheme';
 import { useClientOnlyValue } from '@/components/useClientOnlyValue';
 
-// You can explore the built-in icon families and icons on the web at https://icons.expo.fyi/
 function TabBarIcon(props: {
   name: React.ComponentProps<typeof FontAwesome>['name'];
   color: string;
 }) {
-  return <FontAwesome size={28} style={{ marginBottom: -3 }} {...props} />;
+  return (
+    <FontAwesome
+      size={20}
+      style={{ marginBottom: -3 }}
+      {...props}
+    />
+  );
 }
 
 export default function TabLayout() {
@@ -22,17 +27,30 @@ export default function TabLayout() {
     <Tabs
       screenOptions={{
         tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        // Disable the static render of the header on web
-        // to prevent a hydration error in React Navigation v6.
         headerShown: useClientOnlyValue(false, true),
-      }}>
+      }}
+    >
       <Tabs.Screen
         name="index"
+        options={{ href: null }}
+      />
+
+      <Tabs.Screen
+        name="menu"
         options={{
-          title: 'Tab One',
-          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
+          title: 'Menu',
+          headerShown: false,
+          tabBarIcon: ({ color }) => (
+            <TabBarIcon
+              name="cutlery"
+              color={color}
+            />
+          ),
           headerRight: () => (
-            <Link href="/modal" asChild>
+            <Link
+              href="/modal"
+              asChild
+            >
               <Pressable>
                 {({ pressed }) => (
                   <FontAwesome
@@ -50,8 +68,13 @@ export default function TabLayout() {
       <Tabs.Screen
         name="two"
         options={{
-          title: 'Tab Two',
-          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
+          title: 'Orders',
+          tabBarIcon: ({ color }) => (
+            <TabBarIcon
+              name="list"
+              color={color}
+            />
+          ),
         }}
       />
     </Tabs>

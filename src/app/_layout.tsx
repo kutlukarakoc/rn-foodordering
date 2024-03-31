9720b51cf8bb5fbff3179efaa6f3f71adc5e7631
kutlukarakoc <karakockutlu@gmail.com>
2024-03-31 23:18:03 +0300

    feat: wrapped layout with cart provider

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,11 +1,17 @@
 import FontAwesome from '@expo/vector-icons/FontAwesome';
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from '@react-navigation/native';
 import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
 import { useEffect } from 'react';
 
 import { useColorScheme } from '@/components/useColorScheme';
+
+import CartProvider from '@/providers/CartProvider';
 
 export {
   // Catch any errors thrown by the Layout component.
@@ -49,10 +55,18 @@ function RootLayoutNav() {
 
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="cart" options={{ presentation: 'modal' }} />
-      </Stack>
+      <CartProvider>
+        <Stack>
+          <Stack.Screen
+            name="(tabs)"
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="cart"
+            options={{ presentation: 'modal' }}
+          />
+        </Stack>
+      </CartProvider>
     </ThemeProvider>
   );
 }

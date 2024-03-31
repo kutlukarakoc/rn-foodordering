9e0c1ddd3df028771a3627e1f711b312cf059273
kutlukarakoc <karakockutlu@gmail.com>
2024-03-31 23:19:18 +0300

    feat: completed Cart page

--- src/app/cart.tsx
+++ src/app/cart.tsx
@@ -1,13 +1,21 @@
-import { View, Text, Platform } from 'react-native'
-import React from 'react'
-import { StatusBar } from 'expo-status-bar'
+import { View, Text, Platform, FlatList } from 'react-native';
+import React from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { useCart } from '@/providers/CartProvider';
+import CartListItem from '@/components/CartListItem';
 
 export default function Cart() {
-	return (
-		<View>
-			<Text>Cart</Text>
+  const { items } = useCart();
 
-			<StatusBar style={Platform.OS === 'ios' ? 'light' : 'auto'} />
-		</View>
-	)
+  return (
+    <View>
+      <FlatList
+        data={items}
+        renderItem={({ item }) => <CartListItem cartItem={item} />}
+				contentContainerStyle={{ padding: 10, gap: 10 }}
+      />
+
+      <StatusBar style={Platform.OS === 'ios' ? 'light' : 'auto'} />
+    </View>
+  );
 }

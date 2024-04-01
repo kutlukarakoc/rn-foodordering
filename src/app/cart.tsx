c4ed2ba4413e83e741302dfddb996c4cfbce5d4a
kutlukarakoc <karakockutlu@gmail.com>
2024-04-01 23:43:57 +0300

    feat: added not items ui to cart

--- src/app/cart.tsx
+++ src/app/cart.tsx
@@ -1,4 +1,5 @@
-import { View, Text, Platform, FlatList } from 'react-native';
+import { View, Text, Platform, FlatList, StyleSheet } from 'react-native';
+import { Link } from 'expo-router';
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { useCart } from '@/providers/CartProvider';
@@ -9,17 +10,62 @@ export default function Cart() {
   const { items, total } = useCart();
 
   return (
-    <View style={{ padding: 10 }}>
-      <FlatList
-        data={items}
-        renderItem={({ item }) => <CartListItem cartItem={item} />}
-        contentContainerStyle={{ padding: 10, gap: 10 }}
-      />
-
-			<Text style={{ marginTop: 20, fontSize: 20, fontWeight: '500' }}>${total}</Text>
-      <Button text="Checkout" />
+    <View style={styles.container}>
+      {items.length ? (
+        <>
+          <FlatList
+            data={items}
+            renderItem={({ item }) => <CartListItem cartItem={item} />}
+            contentContainerStyle={styles.productList}
+          />
+          <Text style={styles.total}>${total}</Text>
+          <Button text="Checkout" />
+        </>
+      ) : (
+        <View
+          style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}
+        >
+          <Text style={styles.noProducts}>There is no pizza in your cart</Text>
+          <Text style={[styles.noProducts, { fontSize: 17 }]}>
+            Check out our delicious pizzas
+          </Text>
+          <Link
+            href="/menu/"
+            style={styles.link}
+          >
+            <Text>Menu</Text>
+          </Link>
+        </View>
+      )}
 
       <StatusBar style={Platform.OS === 'ios' ? 'light' : 'auto'} />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 10,
+    flex: 1,
+  },
+  productList: {
+    padding: 10,
+    gap: 10,
+  },
+  total: {
+    marginTop: 20,
+    fontSize: 20,
+    fontWeight: '500',
+  },
+  noProducts: {
+    marginTop: 20,
+    textAlign: 'center',
+    fontSize: 20,
+    fontWeight: '600',
+  },
+  link: {
+    marginTop: 30,
+    textDecorationLine: 'underline',
+    fontSize: 17,
+  },
+});

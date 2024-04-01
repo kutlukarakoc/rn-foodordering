ffb527975e755df8a3818a2b37cc20ec908f0ac9
kutlukarakoc <karakockutlu@gmail.com>
2024-04-02 00:01:40 +0300

    feat: added stack screen for admin

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -57,6 +57,10 @@ function RootLayoutNav() {
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
       <CartProvider>
         <Stack>
+				<Stack.Screen
+            name="(admin)"
+            options={{ headerShown: false }}
+          />
           <Stack.Screen
             name="(user)"
             options={{ headerShown: false }}

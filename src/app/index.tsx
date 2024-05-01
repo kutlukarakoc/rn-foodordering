e149a0a8483531836ae07056eeacd96890852728
kutlukarakoc <karakockutlu@gmail.com>
2024-05-01 16:44:00 +0300

    feat: added sign-in sign-up auth ui and route

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -11,6 +11,9 @@ export default function index() {
       <Link href="/(admin)" asChild>
         <Button text="Admin" />
       </Link>
+      <Link href="/sign-in" asChild>
+        <Button text="Sign in" />
+      </Link>
     </View>
   );
 }

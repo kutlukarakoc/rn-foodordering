8dfca82a7e374bb0f66eed8d3b04542e06ecc372
kutlukarakoc <karakockutlu@gmail.com>
2024-05-03 19:05:55 +0300

    fix: removed second title for orders screen

--- src/app/(user)/_layout.tsx
+++ src/app/(user)/_layout.tsx
@@ -49,9 +49,10 @@ export default function TabLayout() {
         }}
       />
       <Tabs.Screen
-        name="two"
+        name="orders"
         options={{
           title: 'Orders',
+					headerShown: false,
           tabBarIcon: ({ color }) => (
             <TabBarIcon
               name="list"

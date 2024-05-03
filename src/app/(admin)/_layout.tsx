8d53f12c2d1bd502f5445edbcee769dec0fa9d85
kutlukarakoc <karakockutlu@gmail.com>
2024-05-03 19:23:26 +0300

    feat: added orders and order detail screen for admin side

--- src/app/(admin)/_layout.tsx
+++ src/app/(admin)/_layout.tsx
@@ -47,8 +47,9 @@ export default function TabLayout() {
         }}
       />
       <Tabs.Screen
-        name="two"
+        name="orders"
         options={{
+					headerShown: false,
           title: 'Orders',
           tabBarIcon: ({ color }) => (
             <TabBarIcon
---
+++ src/app/(admin)/orders/_layout.tsx
@@ -0,0 +1,16 @@
+import Colors from '@/constants/Colors';
+import { FontAwesome } from '@expo/vector-icons';
+import { Link, Stack } from 'expo-router';
+import { Pressable } from 'react-native';
+
+export default function MenuStack() {
+  return (
+    <Stack
+    >
+      <Stack.Screen
+        name="index"
+        options={{ title: 'Orders' }}
+      />
+    </Stack>
+  );
+}

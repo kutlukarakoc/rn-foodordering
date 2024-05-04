4aa9003c720d900ca0091b2a8e230b79e57b0c6b
kutlukarakoc <karakockutlu@gmail.com>
2024-05-04 15:00:03 +0300

    fix: removed unnecessary imports, prettier for admin orders layout

--- src/app/(admin)/orders/_layout.tsx
+++ src/app/(admin)/orders/_layout.tsx
@@ -1,12 +1,8 @@
-import Colors from '@/constants/Colors';
-import { FontAwesome } from '@expo/vector-icons';
-import { Link, Stack } from 'expo-router';
-import { Pressable } from 'react-native';
+import { Stack } from 'expo-router';
 
 export default function MenuStack() {
   return (
-    <Stack
-    >
+    <Stack>
       <Stack.Screen
         name="index"
         options={{ title: 'Orders' }}

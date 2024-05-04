544f4ee67fb4e697e0e06ccdbe27c16adc38b224
kutlukarakoc <karakockutlu@gmail.com>
2024-05-04 15:46:56 +0300

    added top tabs for admin orders

--- src/app/(admin)/orders/_layout.tsx
+++ src/app/(admin)/orders/_layout.tsx
@@ -3,9 +3,13 @@ import { Stack } from 'expo-router';
 export default function MenuStack() {
   return (
     <Stack>
-      <Stack.Screen
+      {/* <Stack.Screen
         name="index"
         options={{ title: 'Orders' }}
+      /> */}
+      <Stack.Screen
+        name="list"
+        options={{ headerShown: false }}
       />
     </Stack>
   );

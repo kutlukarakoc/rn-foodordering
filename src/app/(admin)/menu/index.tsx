28d07b55c6c6d7e1f59f1d4aa6325d8c0dd81a23
kutlukarakoc <karakockutlu@gmail.com>
2024-05-11 15:08:43 +0300

    feat: replaced products with db instead of using dummy data

--- src/app/(admin)/menu/index.tsx
+++ src/app/(admin)/menu/index.tsx
@@ -1,8 +1,18 @@
-import { FlatList } from 'react-native';
+import { ActivityIndicator, FlatList, Text } from 'react-native';
 import { ProductListItem } from '@/components/ProductListItem';
-import products from 'assets/data/products';
+import { useProductList } from '@/api/products';
 
 export default function MenuScreen() {
+	const { data: products, error, isLoading } = useProductList();
+
+  if (isLoading) {
+    return <ActivityIndicator />;
+  }
+
+  if (error) {
+    return <Text>Failed to fetch to products</Text>;
+  }
+
   return (
     <FlatList
       data={products}
--- src/app/(user)/menu/index.tsx
+++ src/app/(user)/menu/index.tsx
@@ -1,15 +1,25 @@
-import { FlatList } from 'react-native';
+import { ActivityIndicator, FlatList, Text } from 'react-native';
 import { ProductListItem } from '@/components/ProductListItem';
-import products from 'assets/data/products';
+import { useProductList } from '@/api/products';
 
 export default function MenuScreen() {
+  const { data: products, error, isLoading } = useProductList();
+
+  if (isLoading) {
+    return <ActivityIndicator />;
+  }
+
+  if (error) {
+    return <Text>Failed to fetch to products</Text>;
+  }
+
   return (
     <FlatList
       data={products}
       renderItem={({ item }) => <ProductListItem product={item} />}
-			numColumns={2}
-			contentContainerStyle={{ gap: 10, padding: 10 }}
-			columnWrapperStyle={{ gap: 10 }}
+      numColumns={2}
+      contentContainerStyle={{ gap: 10, padding: 10 }}
+      columnWrapperStyle={{ gap: 10 }}
     />
   );
 }

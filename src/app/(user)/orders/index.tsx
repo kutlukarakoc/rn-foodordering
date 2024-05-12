6bc8d7e5d81c4c8f3809734a482d05a97d2a12f4
kutlukarakoc <karakockutlu@gmail.com>
2024-05-12 15:25:52 +0300

    feat: added order list for users side

--- src/app/(user)/orders/index.tsx
+++ src/app/(user)/orders/index.tsx
@@ -1,8 +1,14 @@
-import { FlatList } from 'react-native';
-import orders from 'assets/data/orders';
+import { ActivityIndicator, FlatList, Text } from 'react-native';
 import OrderListItem from '@/components/OrderListItem';
+import { useUserOrdersList } from '@/api/orders';
 
 export default function OrdersScreen() {
+  const { data: orders, isLoading, error } = useUserOrdersList();
+
+  if (isLoading) return <ActivityIndicator />;
+
+  if (error) return <Text>Failed to fetch orders</Text>;
+
   return (
     <FlatList
       data={orders}

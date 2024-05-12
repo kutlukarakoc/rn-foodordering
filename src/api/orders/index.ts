a614bceaa0751d8414b48e273e0e1973bd9e1d43
kutlukarakoc <karakockutlu@gmail.com>
2024-05-12 15:38:14 +0300

    feat: added useOrderDetails hook

--- src/api/orders/index.ts
+++ src/api/orders/index.ts
@@ -42,3 +42,22 @@ export const useUserOrdersList = () => {
     },
   });
 };
+
+export const useOrderDetails = (id: number) => {
+  return useQuery({
+    queryKey: ['orders', id],
+    queryFn: async () => {
+      const { data, error } = await supabase
+        .from('orders')
+        .select('*')
+        .eq('id', id)
+        .single();
+
+      if (error) {
+        throw new Error(error.message);
+      }
+      return data;
+    },
+  });
+};
+

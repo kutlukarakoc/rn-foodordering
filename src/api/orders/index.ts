0ef0684cf4d1b52ce16930944068342741dcb407
kutlukarakoc <karakockutlu@gmail.com>
2024-05-12 17:21:04 +0300

    feat: added nested query for useOrderDetails

--- src/api/orders/index.ts
+++ src/api/orders/index.ts
@@ -52,7 +52,7 @@ export const useOrderDetails = (id: number) => {
     queryFn: async () => {
       const { data, error } = await supabase
         .from('orders')
-        .select('*')
+        .select('*, order_items(*, products(*))')
         .eq('id', id)
         .single();
 

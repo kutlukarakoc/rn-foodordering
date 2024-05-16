a5e1b9232757b9fdacfc54879e62c5b213ba7fdb
kutlukarakoc <karakockutlu@gmail.com>
2024-05-16 20:37:43 +0300

    added basic notification

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -14,6 +14,7 @@ import { useColorScheme } from '@/components/useColorScheme';
 import CartProvider from '@/providers/CartProvider';
 import AuthProvider from '@/providers/AuthProvider';
 import QueryProvider from '@/providers/QueryProvider';
+import NotificationProvider from '@/providers/NotificationProvider';
 
 export {
   // Catch any errors thrown by the Layout component.
@@ -59,26 +60,28 @@ function RootLayoutNav() {
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
       <AuthProvider>
         <QueryProvider>
-          <CartProvider>
-            <Stack>
-              <Stack.Screen
-                name="(admin)"
-                options={{ headerShown: false }}
-              />
-              <Stack.Screen
-                name="(user)"
-                options={{ headerShown: false }}
-              />
-              <Stack.Screen
-                name="(auth)"
-                options={{ headerShown: false }}
-              />
-              <Stack.Screen
-                name="cart"
-                options={{ presentation: 'modal' }}
-              />
-            </Stack>
-          </CartProvider>
+          <NotificationProvider>
+            <CartProvider>
+              <Stack>
+                <Stack.Screen
+                  name="(admin)"
+                  options={{ headerShown: false }}
+                />
+                <Stack.Screen
+                  name="(user)"
+                  options={{ headerShown: false }}
+                />
+                <Stack.Screen
+                  name="(auth)"
+                  options={{ headerShown: false }}
+                />
+                <Stack.Screen
+                  name="cart"
+                  options={{ presentation: 'modal' }}
+                />
+              </Stack>
+            </CartProvider>
+          </NotificationProvider>
         </QueryProvider>
       </AuthProvider>
     </ThemeProvider>
---
+++ src/providers/NotificationProvider.tsx
@@ -0,0 +1,56 @@
+import { PropsWithChildren, useEffect, useRef, useState } from 'react';
+import { registerForPushNotificationsAsync } from '@/lib/notifications';
+import * as Notifications from 'expo-notifications';
+
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: false,
+    shouldSetBadge: false,
+  }),
+});
+
+const NotificationProvider = ({ children }: PropsWithChildren) => {
+  const [expoPushToken, setExpoPushToken] = useState('');
+  const [notification, setNotification] = useState<
+    Notifications.Notification | undefined
+  >(undefined);
+  const notificationListener = useRef<Notifications.Subscription>();
+  const responseListener = useRef<Notifications.Subscription>();
+
+  useEffect(() => {
+    registerForPushNotificationsAsync()
+      .then((token) => setExpoPushToken(token ?? ''))
+      .catch((error: any) => setExpoPushToken(`${error}`));
+
+    notificationListener.current =
+      Notifications.addNotificationReceivedListener((notification) => {
+        setNotification(notification);
+      });
+
+    responseListener.current =
+      Notifications.addNotificationResponseReceivedListener((response) => {
+        console.log(response);
+      });
+
+    return () => {
+      if (notificationListener.current) {
+        notificationListener.current &&
+          Notifications.removeNotificationSubscription(
+            notificationListener.current,
+          );
+      }
+
+      if (responseListener.current) {
+        responseListener.current &&
+          Notifications.removeNotificationSubscription(
+            responseListener.current,
+          );
+      }
+    };
+  }, []);
+
+  return <>{children}</>;
+};
+
+export default NotificationProvider;

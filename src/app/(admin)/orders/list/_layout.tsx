8c5a507590933e49081632dd4272ea1dfcc06425
kutlukarakoc <karakockutlu@gmail.com>
2024-05-17 16:26:38 +0300

    feat: updated OrderListNavigator to use color scheme for safe area bg

--- src/app/(admin)/orders/list/_layout.tsx
+++ src/app/(admin)/orders/list/_layout.tsx
@@ -1,15 +1,28 @@
 import { withLayoutContext } from 'expo-router';
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import { useColorScheme } from 'react-native';
+import Colors from '@/constants/Colors';
 
 const TopTabs = withLayoutContext(createMaterialTopTabNavigator().Navigator);
 
 export default function OrderListNavigator() {
+  const colorScheme = useColorScheme();
+
   return (
-    <SafeAreaView style={{ flex: 1, backgroundColor: 'white' }} edges={['top']}>
+    <SafeAreaView
+      style={{
+        flex: 1,
+        backgroundColor: Colors[colorScheme ?? 'light'].background,
+      }}
+      edges={['top']}
+    >
       <TopTabs>
-				<TopTabs.Screen name='index' options={{ title: 'Active' }} />
-			</TopTabs>
+        <TopTabs.Screen
+          name="index"
+          options={{ title: 'Active' }}
+        />
+      </TopTabs>
     </SafeAreaView>
   );
 }
